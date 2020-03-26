a520b872ac1a3b2fdcc49c0b73844e4eefee8e2a
Jonatas Fontele <jonatasfontele@gmail.com>
2020-03-26 17:23:22 -0300

    Update 0110_Consecutives.py

--- 0110_Consecutives.py
+++ 0110_Consecutives.py
@@ -7,16 +7,16 @@ V = input().split() # Lista de String
 for i in range(len(V)): # Transformar todos os elementos da lista de string para inteiros
     V[i] = int(V[i])
 
-longestSequence = 0  # Tamanho da maior sequencia
-currentSequence = 1  # Tamanho da sequencia atual (estamos representando que o primeiro elemento representa uma sequência de tamanho 1)
+longest_sequence = 0  # Tamanho da maior sequencia
+current_sequence = 1  # Tamanho da sequencia atual (estamos representando que o primeiro elemento representa uma sequência de tamanho 1)
 
 for i in range(1, len(V)):
     if V[i] == V[i - 1]:
-        currentSequence += 1  # Se o número atual é igual ao anterior o tamanho da sequência aumenta
+        current_sequence += 1  # Se o número atual é igual ao anterior o tamanho da sequência aumenta
     else:
-        currentSequence = 1  # Se o número é diferente, começamos uma nova sequência
+        current_sequence = 1  # Se o número é diferente, começamos uma nova sequência
 
-    if (currentSequence > longestSequence):
-        longestSequence = currentSequence  # Se o tamanho da sequência que estamos avaliando é maior do que a que tinhamos antes, atualizamos o tamanho da maior sequência.
+    if (current_sequence > longest_sequence):
+        longest_sequence = current_sequence  # Se o tamanho da sequência que estamos avaliando é maior do que a que tinhamos antes, atualizamos o tamanho da maior sequência.
 
-print(longestSequence)
+print(longest_sequence)
