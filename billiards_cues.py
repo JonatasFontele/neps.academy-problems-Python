fe46e09523f439910043dedc72381913143c9f1e
Jonatas Fontele <jonatasfontele@gmail.com>
2021-04-21 13:28:03 -0300

    Add files via upload

---
+++ billiards_cues.py
@@ -0,0 +1,49 @@
+# Problema Tacos de Bilhar
+# OBI 2016 - Primeira Fase - Nível 1
+from collections import Counter
+
+
+def consult_cue(stock_queries):
+    made = 0
+    # Creates a container with Counter({number: quantity})
+    count_queries = Counter(stock_queries)
+    # Creates a dictionary with {quantities_repeated: quantity}
+    count_amount = dict(Counter(count_queries.values()))
+    for key, value in dict(count_amount).items():
+        # if quantities_repeated is odd, we add 1 & multiply for the quantity. Since Mr Jorge makes 2 cues and stores 1.
+        if key % 2:
+            made += (key + 1) * value
+        # if quantities_repeated is even, we multiply for the quantity. Since Mr Jorge makes 2 cues and sends both.
+        else:
+            made += key * value
+    return made
+
+    # This is a solution with Time Limit Exceeded complexity O(n^2)
+    #
+    # for consult in stock_queries:
+    #   if consult in stock:
+    #       stock.remove(consult)
+    #   else:
+    #       stock.append(consult)
+    #       made += 2
+    # return made
+
+
+def validate(c, stock_queries):
+    constraints = True
+    for number in stock_queries:
+        if not (number in range(1, (10 ** 6) + 1)):
+            constraints = False
+            break
+    if constraints and 1 <= c <= 10**5:
+        print(consult_cue(stock_queries))
+
+
+def main():
+    c = int(input())
+    stock_queries = list(map(int, input().split()))
+    validate(c, stock_queries)
+
+
+if __name__ == "__main__":
+    main()
