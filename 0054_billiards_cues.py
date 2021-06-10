460c4041482b661e31607af968dc64d055231c3d
Jonatas Fontele <jonatasfontele@gmail.com>
2021-06-10 16:31:27 -0300

    Added some other solutions. For loop still faster.

--- 0054_billiards_cues.py
+++ 0054_billiards_cues.py
@@ -3,47 +3,56 @@
 from collections import Counter
 
 
+# This is the fastest solution
 def consult_cue(stock_queries):
     made = 0
-    # Creates a container with Counter({number: quantity})
+    # Creates a container that stores elements as a dictionary with Counter({number: quantity})
     count_queries = Counter(stock_queries)
-    # Creates a dictionary with {quantities_repeated: quantity}
-    count_amount = dict(Counter(count_queries.values()))
-    for key, value in dict(count_amount).items():
+    # Creates a container that stores elements as a dictionary with {quantities_repeated: quantity}
+    count_amount = Counter(count_queries.values())
+    for key, value in count_amount.items():
         # quantities_repeated is odd. Ex.: 3 x 100 cm (4 made) + 3 x 80 cm (4 made) + 3 x 50 cm (4 made) = 12 cues made
         if key % 2:
             made += (key + 1) * value
         # quantities_repeated is even. Ex.: 2 x 100 cm (2 made) + 2 x 80 cm (2 made) + 2 x 50 cm (2 made) = 6 cues made
         else:
             made += key * value
-    return made
+    return print(made)
 
-    # This is a solution with Time Limit Exceeded complexity O(n^2)
-    #
-    # for consult in stock_queries:
-    #   if consult in stock:
-    #       stock.remove(consult)
-    #   else:
-    #       stock.append(consult)
-    #       made += 2
-    # return made
+# This is the second fastest solution
+# def consult_cue2(stock_queries):
+#     count_queries = Counter(stock_queries)
+#     count_amount = Counter(count_queries.values())
+#     return sum([(key + 1) * value if key % 2 else key * value for key, value in count_amount.items()])
 
+# This is the third fastest solution
+# def consult_cue3(stock_queries):
+#     count_queries = Counter(stock_queries)
+#     count_amount = Counter(count_queries.values())
+#     return sum(list(map(lambda kv: my_func(kv[0], kv[1]), count_amount.items())))
+# def my_func(key, value):
+#     if key % 2:
+#         return (key + 1) * value
+#     return key * value
 
-# This validation makes it slower, but still less than 1 sec for 10^5
-def validate(c, stock_queries):
-    constraints = True
-    for number in stock_queries:
-        if not (number in range(1, (10 ** 6) + 1)):
-            constraints = False
-            break
-    if constraints and 1 <= c <= 10**5:
-        print(consult_cue(stock_queries))
+# This is a solution with Time Limit Exceeded complexity O(n^2) the slower
+# It's better for short input and a great time of repetitions of the function in the process.
+# def consult_cue4(stock_queries):
+#   stock = []
+#   made = 0
+#   for consult in stock_queries:
+#       if consult in stock:
+#           stock.remove(consult)
+#       else:
+#           stock.append(consult)
+#           made += 2
+#   return made
 
 
 def main():
-    c = int(input())
+    int(input())
     stock_queries = list(map(int, input().split()))
-    validate(c, stock_queries)
+    consult_cue(stock_queries)
 
 
 if __name__ == "__main__":
