4b5c692ce62425ffc61d8d649443a549d4a65f93
Jonatas Dourado <jonatasfontele@gmail.com>
2022-03-11 04:58:43 -0300

    Lâmpadas

--- 0052_lamps.py
+++ 0052_lamps.py
@@ -1,16 +1,14 @@
-# Lâmpadas
+# Problema Lâmpadas
 # OBI 2016 - Primeira Fase - Nível Júnior
 N = int(input())
-I = input().split()
-I = [int(I[i]) for i in range(len(I))] # Transformar todos os elementos da lista de string para inteiros
-A = 0 # Lâmpada A apagada
-B = 0 # Lâmpada B apagada
+I = map(int, input().split())
+A, B = 0, 0  # Lâmpada A e B apagadas
 
-for i in range(N):
-    if I[i] == 1: # Interruptor I1
+for i in I:
+    if i == 1:  # Interruptor I1
         A += 1
-    if I[i] == 2: # Interruptor I2
+    if i == 2:  # Interruptor I2
         A += 1
         B += 1
-print(A % 2) # Módulo/resto da divisão por 2 gera 0 ou 1 para par ou ímpar
+print(A % 2)  # Módulo/resto da divisão por 2 gera 0 ou 1 para par ou ímpar
 print(B % 2)
