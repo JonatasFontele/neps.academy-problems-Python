10b44182b44cfd84c6bb4cd45a79a2b7c277597e
Jonatas Dourado <jonatasfontele@gmail.com>
2022-03-11 04:18:39 -0300

    Premio do Milhão

--- 0048_million_prize.py
+++ 0048_million_prize.py
@@ -2,10 +2,9 @@
 # OBI 2015 - Primeira Fase - Nível Júnior
 N = int(input())
 sum = 0
-enough = False
 for i in range(N):
     A = int(input())
     sum += A
-    if sum >= 1000000 and not enough:
+    if sum >= 1000000:
         print(i + 1)
-        enough = True
+        break
